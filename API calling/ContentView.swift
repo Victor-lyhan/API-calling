7b8e86cdee8777af7a0267e1ab4cdd645379d1d3
Victor Han <liyanghan1112@gmail.com>
2022-07-26 14:47:07 -0500

    done

--- API calling/ContentView.swift
+++ API calling/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var foods = [Food]()
+    @State private var showingAlert = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(foods) { food in
+                VStack{
+                    NavigationLink {
+                        Text("\(food.price) dollars")
+                        Text(food.description)
+                    } label: {
+                        Text(food.name)
+                    }
+                }
+            }
+            .navigationTitle("Foods")
+        }
+        .onAppear {
+            getFoods()
+        }
+        .alert(isPresented: $showingAlert){
+            Alert(title: Text("Loading Error"),
+                  message: Text("there was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func getFoods() {
+        let apiKey = "?rapidapi-key=d582ffb81bmsh2b53131e0f4dbedp18510bjsnea5f7f34cb91"
+        let query = "https://pizza-and-desserts.p.rapidapi.com/desserts\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                    let contents = json.arrayValue
+                    for item in contents {
+                        let name = item["name"].stringValue
+                        let price = item["price"].stringValue
+                        let description = item["description"].stringValue
+                        let food = Food(name: name, price: price, description: description)
+                        foods.append(food)
+                    }
+                    return
+            }
+        }
+        showingAlert = true
     }
 }
 
@@ -18,4 +59,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Food: Identifiable {
+    let id = UUID()
+    var name = ""
+    var price = ""
+    var description = ""
 }
