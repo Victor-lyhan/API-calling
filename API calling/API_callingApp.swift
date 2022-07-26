f60880d2d6cd68794e3c6a6d86adc5fd5b1b7196
Victor Han <liyanghan1112@gmail.com>
2022-07-26 13:54:36 -0500

    Initial Commit

---
+++ API calling/API_callingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_callingApp.swift
+//  API calling
+//
+//  Created by Victor Han on 2022/7/26.
+//
+
+import SwiftUI
+
+@main
+struct API_callingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
